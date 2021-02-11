20f010885a29053dc4e0e820134f616051138cf0
John Gruber <jgruber@f5.com>
2021-02-11 15:49:29 -0600

    remove apikey references and fixed resource group name

--- provider.tf
+++ provider.tf
@@ -1,14 +1,12 @@
 terraform {
   required_providers {
     ibm = {
-      source = "IBM-Cloud/ibm"
+      source  = "IBM-Cloud/ibm"
     }
   }
 }
 
 provider "ibm" {
-  /* Uncomment ibmcloud_api_key while testing from CLI */
-  ibmcloud_api_key = var.api_key
   generation       = 2
   region           = var.region
   ibmcloud_timeout = 300
