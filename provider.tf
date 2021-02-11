b69f36a2a2ef053a25af1470dcae25f804f1c6c9
f5craigsca <c.scarborough@f5.com>
2021-02-11 13:05:24 -0500

    commented out API key in provider.tf and added testing.tfvars

--- provider.tf
+++ provider.tf
@@ -25,8 +25,8 @@ variable "zone" {
 }
 
 
-provider "ibm" {
-  ibmcloud_api_key = var.api_key
-  generation       = 2
-  region           = var.region
-}
+#provider "ibm" {
+#  ibmcloud_api_key = var.api_key
+#  generation       = 2
+#  region           = var.region
+#}
