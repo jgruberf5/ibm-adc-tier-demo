8dd4828f94f9f36c8f1ee59ff9c2e3d289d216ad
f5craigsca <c.scarborough@f5.com>
2021-02-11 13:35:09 -0500

    added required provider

--- provider.tf
+++ provider.tf
@@ -24,9 +24,18 @@ variable "zone" {
   description = "Zones in each region to test."
 }
 
+terraform {
+  required_providers {
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+    }
+  }
+}
 
-#provider "ibm" {
+provider "ibm" {
+/* Uncomment ibmcloud_api_key while testing from CLI */
 #  ibmcloud_api_key = var.api_key
-#  generation       = 2
-#  region           = var.region
-#}
+  generation       = 2
+  region           = var.region
+  ibmcloud_timeout = 300
+}
